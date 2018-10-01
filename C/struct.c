7eff1874a1d4ea470634f99bf0be0ecbc89c46aa
Wine93 <wine93.info@gmail.com>
2018-10-01 10:16:28 +0000

    C: structure memory alignment.

---
+++ C/struct.c
@@ -0,0 +1,33 @@
+/*=============================================================================
+ * Date:           2018-10-01
+ * Author:         wine93 <wine93.info@gmail.com>
+ * Description:    Structure memory alignment
+ * =============================================================================*/
+
+#include <stdio.h>
+
+
+struct x {
+    int       a;
+    unsigned  u1:1;
+    int       b;
+    unsigned  u2:1;
+};
+
+
+struct y {
+    int       a;
+    int       b;
+    unsigned  u1:1;
+    unsigned  u2:1;
+};
+
+
+int
+main()
+{
+    printf ("sizeof(struct x): %d\n", sizeof(struct x));
+    printf ("sizeof(struct y): %d\n", sizeof(struct y));
+
+    return 0;
+}
